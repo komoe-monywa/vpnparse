b0237494898f4b4dd53b8aa5c5d240f72f3cd07f
komoe (Monywa) <nyeinm910@gmail.com>
2026-02-10 00:41:15 +0630

    Update main.go

--- main.go
+++ main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"github.com/komoe-monywa/uri-to-json/pkgs/cmd"
-	_ "github.com/komoe-monywa/uri-to-json/pkgs/outbound/sing"
-	_ "github.com/komoe-monywa/uri-to-json/pkgs/outbound/xray"
-	_ "github.com/komoe-monywa/uri-to-json/pkgs/parser"
+	"github.com/komoe-monywa/vpnparse/pkgs/cmd"
+	_ "github.com/komoe-monywa/vpnparse/pkgs/outbound/sing"
+	_ "github.com/komoe-monywa/vpnparse/pkgs/outbound/xray"
+	_ "github.com/komoe-monywa/vpnparse/pkgs/parser"
 )
 
 func main() {
